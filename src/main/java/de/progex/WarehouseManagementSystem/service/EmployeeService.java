ae053567511b3bcc85585ffbaced55bbfcecc588
Ibrahim <akguel@stud.fra-uas.de>
2021-05-06 16:14:44 +0200

    update

--- src/main/java/de/progex/WarehouseManagementSystem/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package de.progex.WarehouseManagementSystem.service;
-
-import de.progex.WarehouseManagementSystem.tables.Employee;
-
-public interface EmployeeService {
-
-    public Employee getEmployeeById(int id);
-
-}
