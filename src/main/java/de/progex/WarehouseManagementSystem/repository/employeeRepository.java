9fefaafbb7cb6cb00d42fa5ff7bdb9d4000cb3f8
Ibrahim <akguel@stud.fra-uas.de>
2021-05-06 15:28:44 +0200

    jpa schnittstelle update

--- src/main/java/de/progex/WarehouseManagementSystem/repository/employeeRepository.java
+++ src/main/java/de/progex/WarehouseManagementSystem/repository/employeeRepository.java
@@ -1,2 +1,7 @@
-package de.progex.WarehouseManagementSystem.repository;public interface employeeRepository {
+package de.progex.WarehouseManagementSystem.repository;
+
+import de.progex.WarehouseManagementSystem.tables.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface employeeRepository extends JpaRepository<Long, Employee> {
 }
